3ce2b1c78be50b54de8b459e0df44b52177489ec
tomcze15 <tomasz.czerwinski.1997@gmail.com>
2023-04-22 20:02:09 +0200

    🔧 Add eslint and tailwindcss config

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,14 @@
 import { ReactElement } from "react";
 
 function App(): ReactElement {
-  return <div className="text-3xl font-bold underline">Hello world</div>;
+  return (
+    <div className="text-xs font-bold underline lg:text-3xl">
+      Hello world
+      <button type="button" className="rounded-lg hover:bg-violet-700">
+        Click me
+      </button>
+    </div>
+  );
 }
 
 export default App;
